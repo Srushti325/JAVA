0d6174cd97b5f76fd1737e89099bbc85f4209304
Srushti325 <shrushtikulkarni828@gmail.com>
2025-08-10 09:10:42 +0530

    new change

--- ImplementHashing.java
+++ ImplementHashing.java
@@ -73,5 +73,43 @@
 //     }
 // }
 
+// Find Iternary for tickets
+/*
+ * "chennai" -> "Bengluru"
+ * "Mumbai" -> "Delhi"
+ * "Goa" -> "Chennai"
+ * "Delhi" -> "Goa"
+ */
+import java.util.*;
+public class ImplementHashing {
+    public static String getStart(HashMap<String, String> tick) {
+        HashMap<String, String> revMap = new HashMap<>();
+        for(String key : tick.keySet()) {
+            revMap.put(tick.get(key), key);
+        }
+        for(String key : tick.keySet()) {
+            if(!revMap.containsKey(key)) {
+                return key;
+            }
+        }
+        return null;
+    }
+    public static void main(String args[]) {
+        HashMap<String, String> tickets = new HashMap<>();
+        tickets.put("Chennai", "Bengluru");
+        tickets.put("Mumbai", "Delhi");
+        tickets.put("Goa", "Chennai");
+        tickets.put("Delhi", "Goa");
+
+        String start = getStart(tickets);
+
+        while(tickets.containsKey(start)) {
+            System.out.print(start + "->");
+            start = tickets.get(start);
+        }
+        System.out.print(start);
+    }
+}
+
 
 
