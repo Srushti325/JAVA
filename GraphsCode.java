23f3da67611ba8e9267b50ac93ae67d8e42bb0af
Srushti325 <shrushtikulkarni828@gmail.com>
2025-08-11 23:25:11 +0530

    new change

--- GraphsCode.java
+++ GraphsCode.java
@@ -1,12 +1,61 @@
+// Unweighted Graph
+// import java.util.*;
+// public class GraphsCode {
+//     static class Edge {
+//         int src;
+//         int dest;
+
+//         public Edge(int s, int d) {
+//             this.src = s;
+//             this.dest = d;
+//         }
+//     }
+
+//     public static void creatGraph(ArrayList<Edge> graph[]) {
+//         for(int i=0; i<graph.length; i++) {
+//             graph[i] = new ArrayList<Edge>();
+//         }
+//         // add edges
+//         graph[0].add(new Edge(0, 2));
+
+//         graph[1].add(new Edge(1, 2));
+//         graph[1].add(new Edge(1, 3));
+
+//         graph[2].add(new Edge(2, 0));
+//         graph[2].add(new Edge(2, 1));
+//         graph[2].add(new Edge(2, 3));
+
+//         graph[3].add(new Edge(3, 1));
+//         graph[3].add(new Edge(3, 2));
+//     }
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter number of vertices: ");
+//         int V = sc.nextInt();
+//         ArrayList<Edge> graph [] = new ArrayList[V];
+//         creatGraph(graph);
+//         // Print neighbours
+//         System.out.print("Enter the vertix who's neighbour's need to be printed: ");
+//         int x = sc.nextInt();
+//         for(int i=0; i<graph[x].size(); i++) {
+//             Edge e = graph[x].get(i);
+//             System.out.println(e.dest);
+//         }
+//     }
+// }
+
+// Weighted Graph
 import java.util.*;
 public class GraphsCode {
     static class Edge {
         int src;
         int dest;
+        int weight;
 
-        public Edge(int s, int d) {
+        public Edge(int s, int d, int w) {
             this.src = s;
             this.dest = d;
+            this.weight = w;
         }
     }
 
@@ -15,17 +64,17 @@ public class GraphsCode {
             graph[i] = new ArrayList<Edge>();
         }
         // add edges
-        graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 2, 2));
 
-        graph[1].add(new Edge(1, 2));
-        graph[1].add(new Edge(1, 3));
+        graph[1].add(new Edge(1, 2, 3));
+        graph[1].add(new Edge(1, 3, -1));
 
-        graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 1));
-        graph[2].add(new Edge(2, 3));
+        graph[2].add(new Edge(2, 0, 2));
+        graph[2].add(new Edge(2, 1, 3));
+        graph[2].add(new Edge(2, 3, 10));
 
-        graph[3].add(new Edge(3, 1));
-        graph[3].add(new Edge(3, 2));
+        graph[3].add(new Edge(3, 1, -1));
+        graph[3].add(new Edge(3, 2, 10));
     }
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
