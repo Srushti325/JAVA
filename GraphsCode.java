15297d29f179a90b1ccf9fb06fb19f62e1b44d39
Srushti325 <shrushtikulkarni828@gmail.com>
2025-08-12 10:31:48 +0530

    new change

--- GraphsCode.java
+++ GraphsCode.java
@@ -94,6 +94,76 @@
 
 
 // BFS
+// import java.util.*;
+// public class GraphsCode {
+//     static class Edge {
+//         int src;
+//         int dest;
+
+//         public Edge(int s, int d) {
+//             this.src = s;
+//             this.dest = d;
+//         }
+//     }
+
+//     public static void creatGraph(ArrayList<Edge> graph[]) {
+//         for(int i=0; i<graph.length; i++) {
+//             graph[i] = new ArrayList<Edge>();
+//         }
+//         // add edges
+//         graph[0].add(new Edge(0, 1));
+//         graph[0].add(new Edge(0, 2));
+
+//         graph[1].add(new Edge(1, 0));
+//         graph[1].add(new Edge(1, 3));
+
+//         graph[2].add(new Edge(2, 0));
+//         graph[2].add(new Edge(2, 4));
+
+//         graph[3].add(new Edge(3, 1));
+//         graph[3].add(new Edge(3, 4));
+//         graph[3].add(new Edge(3, 5));
+
+//         graph[4].add(new Edge(4, 2));
+//         graph[4].add(new Edge(4, 3));
+//         graph[4].add(new Edge(4, 5));
+
+//         graph[5].add(new Edge(5, 3));
+//         graph[5].add(new Edge(5, 4));
+//         graph[5].add(new Edge(5, 6));
+
+//         graph[6].add(new Edge(6, 5));
+        
+//     }
+
+//     public static void bfs(ArrayList<Edge>[] graph, int V) {
+//         Queue<Integer> q = new LinkedList<>();
+//         boolean visited[] = new boolean[V];
+//         q.add(0);
+
+//         while(!q.isEmpty()) {
+//             int curr = q.remove();
+//             if(visited[curr] == false) {
+//                 System.out.print(curr + " ");
+//                 visited[curr] = true;
+//                 for(int i=0; i<graph[curr].size(); i++) {
+//                     Edge e = graph[curr].get(i);
+//                     q.add(e.dest);
+//                 }
+//             }
+//         }
+//     }
+//     public static void main(String args[]) {
+//         int V = 7;
+//         ArrayList<Edge> graph [] = new ArrayList[V];
+//         creatGraph(graph);
+
+//         bfs(graph, V);
+//     }
+// }
+
+
+// BFS Disconnected Graph
 import java.util.*;
 public class GraphsCode {
     static class Edge {
@@ -136,10 +206,10 @@ public class GraphsCode {
         
     }
 
-    public static void bfs(ArrayList<Edge>[] graph, int V) {
+    public static void bfs(ArrayList<Edge>[] graph, int V, boolean visited[], int start) {
         Queue<Integer> q = new LinkedList<>();
-        boolean visited[] = new boolean[V];
-        q.add(0);
+        
+        q.add(start);
 
         while(!q.isEmpty()) {
             int curr = q.remove();
@@ -157,7 +227,11 @@ public class GraphsCode {
         int V = 7;
         ArrayList<Edge> graph [] = new ArrayList[V];
         creatGraph(graph);
-
-        bfs(graph, V);
+        boolean visited[] = new boolean[V];
+        for(int i=0; i<V; i++) {
+            if(visited[i] == false) {
+                bfs(graph, V, visited, i);
+            }
+        }
     }
 }
